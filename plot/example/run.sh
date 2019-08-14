129afdad6a96ebffa4848dc942515c399c655ef2
Simon Marchi <simon.marchi@efficios.com>
2019-08-14 13:44:07 -0400

    sink.plot: Make it possible to override babeltrace2 executable

--- plot/example/run.sh
+++ plot/example/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BABELTRACE2="${BABELTRACE2:-babeltrace2}"
+
 DATA1="[\"timed\", \"Data 1\", \"math:cos\", \"value\"]"
 DATA2="[\"timed\", \"Data 2\", \"math:sin\", \"value\"]"
 DATA3="[\"interpolated\", \"Curve 1\", \"math:cos\", \"value\", \"math:sin\", \"value\"]"
@@ -12,7 +14,7 @@ PLOT4="[\"Curve 1\", \"x\", \"y\", [$DATA3]]"
 PLOT5="[\"Curve 2\", \"x\", \"y\", [$DATA4]]"
 PLOT6="[\"Both 2\", \"x\", \"y\", [$DATA3, $DATA4]]"
 
-babeltrace2 --plugin-path=.. --component sink.plot.PlotSink 		\
+"$BABELTRACE2" --plugin-path=.. --component sink.plot.PlotSink 		\
 	--params="plots=[$PLOT1,$PLOT2,$PLOT3,$PLOT4,$PLOT5,$PLOT6]"	\
 	"$(pwd)/data"
 
